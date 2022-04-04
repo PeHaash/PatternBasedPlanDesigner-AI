2c17958eda61bc852cfa77c6b1b2289482f59500
Pedram Hashemi <101001723+PeHaash@users.noreply.github.com>
2022-04-04 23:35:47 +0430

    Add files via upload

---
+++ Python Implementation/Room.py
@@ -0,0 +1,7 @@
+# import DisjointSet
+
+class ROOM:
+    def __init__(self, NumberOfSubspaces, RoomCode):
+        pass
+        # self.SubspaceConnections=DisjointSet.DISJOINSET(NumberOfSubspaces)
+        # print(RoomCode," has ",NumberOfSubspaces," subs")
