c04536d656335fabcde25e053ce5e9662ec27e50
Pedram Hashemi <101001723+PeHaash@users.noreply.github.com>
2022-04-04 22:50:47 +0430

    Delete RandomNumberMaker.py

--- Python Implementation/RandomNumberMaker.py
+++
@@ -1,6 +0,0 @@
-import random
-
-## 0:Xpos, 1:Ypos, 2:Xweight, 3:Yweight, 4:ParentCode, 5:SubspaceCode, 6: Has Door?, 7: Has Fenestration?
-
-def GenerateRandom(NumberOfSubspaces):
-	return [[random.random() for j in range(8)] for i in range(NumberOfSubspaces)]
