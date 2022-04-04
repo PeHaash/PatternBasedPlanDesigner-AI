8c7a0a8aba7441cb075aeb435ad6d569a640d771
Pedram Hashemi <101001723+PeHaash@users.noreply.github.com>
2022-04-04 19:20:46 +0430

    Delete 22-03-10 directory

--- 22-03-10/Room.py
+++
@@ -1,7 +0,0 @@
-# import DisjointSet
-
-class ROOM:
-    def __init__(self, NumberOfSubspaces, RoomCode):
-        pass
-        # self.SubspaceConnections=DisjointSet.DISJOINSET(NumberOfSubspaces)
-        # print(RoomCode," has ",NumberOfSubspaces," subs")
